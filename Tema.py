25a25f39c56962055a8b9b6b85be31527be4d5ad
Ursan Catalin <151608815+UrsanCatalin09@users.noreply.github.com>
2025-10-21 00:26:56 +0300

    Add files via upload

---
+++ Tema.py
@@ -0,0 +1,17 @@
+text = "Gigi Becali și-a criticat jucătorii după înfrângerea cu Metaloglobus: „Au făcut bani și nu mai au smerenie"
+
+mijloc = len(text) // 2
+prima_parte = text[:mijloc]
+a_doua_parte = text[mijloc:]
+
+prima_parte = prima_parte.upper().strip()
+
+import string
+
+a_doua_parte = a_doua_parte[::-1]
+a_doua_parte = a_doua_parte.capitalize()
+a_doua_parte = ''.join(c for c in a_doua_parte if c not in ".,!?")
+
+rezulatat = prima_parte + " " + a_doua_parte
+print(rezulatat)
+
